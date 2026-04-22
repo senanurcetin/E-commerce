127c58a63c644d9347ed52bac998bb21e5884e3c
Senanur Çetin <senanur.cetin.work@gmail.com>
2026-04-22 13:28:40 +0000

    latest upgrade dbt

--- models/marts/fct_order_marketing.sql
+++ models/marts/fct_order_marketing.sql
@@ -11,6 +11,7 @@ with orders as (
 final as (
 
     select
+        concat(cast(order_item_id as string), '-', cast(order_id as string)) as order_item_sk,
         order_item_id,
         order_id,
         user_id,
@@ -20,13 +21,11 @@ final as (
         datetime(order_item_created_at, "Europe/Istanbul") as order_item_created_at_local,
         date(datetime(order_item_created_at, "Europe/Istanbul")) as order_date,
         extract(hour from datetime(order_item_created_at, "Europe/Istanbul")) as order_hour,
-
         sale_price,
-        case when order_item_status = 'complete' then sale_price else 0 end as revenue,
-        case when order_item_status = 'returned' then sale_price else 0 end as returned_revenue,
-        case when order_item_status = 'complete' then 1 else 0 end as is_completed_order,
-        case when order_item_status = 'returned' then 1 else 0 end as is_returned_order,
-
+        case when lower(order_item_status) = 'complete' then sale_price else 0 end as revenue,
+        case when lower(order_item_status) = 'returned' then sale_price else 0 end as returned_revenue,
+        case when lower(order_item_status) = 'complete' then 1 else 0 end as is_completed_order,
+        case when lower(order_item_status) = 'returned' then 1 else 0 end as is_returned_order,
         product_name,
         category,
         brand,
@@ -35,12 +34,11 @@ final as (
         country,
         traffic_source,
         case
-            when traffic_source in ('facebook', 'youtube', 'adwords', 'display') then 'paid'
-            when traffic_source in ('email') then 'owned'
-            when traffic_source in ('search', 'organic') then 'organic'
+            when lower(traffic_source) in ('facebook', 'youtube', 'adwords', 'display') then 'paid'
+            when lower(traffic_source) = 'email' then 'owned'
+            when lower(traffic_source) in ('search', 'organic') then 'organic'
             else 'other'
         end as channel_group
-
     from orders
 
 )
